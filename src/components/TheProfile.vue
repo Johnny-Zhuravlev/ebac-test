ff91d53ac5c87448174baf52921bc537c6b0c651
Evgeniy Zhuravlev <supreme465@gmail.com>
2023-01-29 00:16:30 +0300

    first commit

---
+++ src/components/TheProfile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Profile page</h1>
+    <TheProfileAvatar />
+  </div>
+</template>
+
+<script>
+import TheProfileAvatar from './TheProfileAvatar.vue'
+export default {
+  components: {
+    TheProfileAvatar
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/TheProfileAvatar.vue
@@ -0,0 +1,78 @@
+<template>
+  <form class="avatar profile-avatar">
+    <div v-if="avatar" class="avatar__loader">
+      <label for="avatar-loader">
+        <img
+          src="@/assets/img/ava.jpeg"
+          class="avatar__img"
+          alt="user avatar"
+        />
+      </label>
+      <input
+        id="avatar-loader"
+        type="file"
+        name="typeFile"
+        accept="image/png, image/jpeg, image/jpg, image/webp"
+      />
+    </div>
+    <div v-else class="avatar__loader">
+      <label for="avatar-loader">
+        <img
+          src="@/assets/img/avatar__default.png"
+          class="avatar__img"
+          alt="user avatar"
+        />
+        <span class="avatar__txt-plug">Загрузить фото</span>
+      </label>
+      <input
+        id="avatar-loader"
+        type="file"
+        name="typeFile"
+        accept="image/png, image/jpeg, image/jpg, image/webp"
+      />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      avatar: '@/assets/img/ava.jpeg'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.avatar {
+  &__loader {
+    display: flex;
+    width: 220px;
+    height: 220px;
+    overflow: hidden;
+    border-radius: 100%;
+    position: relative;
+  }
+
+  &__img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    cursor: pointer;
+  }
+
+  &__txt-plug {
+    color: #ccc;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+  }
+
+  input[type='file'] {
+    display: none;
+  }
+}
+</style>
